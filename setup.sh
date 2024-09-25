d3819037034204edbcb46244c63e4325a645fa64
John Hanekom <144270636+john-hanekom-1@users.noreply.github.com>
2024-09-25 18:32:50 +0000

    temp setup.sh update
    restarting pc

--- setup.sh
+++ setup.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
+
 # Created by: John Hanekom
 # Created on: Sept 2024
 # setup.sh file
 
-set -e  # Exit immediately on non-zero exit code
+# Setup a safe Bash scripting environment
+
+set -o errexit # Exit immediately on non-zero exit code
+set -o noclobber # Prevent files being deleted unless forced
+set -o nounset # Can't remember what this does
+set -o pipefail # Do not wait for piped commands to fail before failing
+
+IFS=$'\n\r' # Set the input field separator
+PATH="$(getconf PATH)"; export PATH
+
+command unalias -a # Remove any aliases
+hash -r # Clear the hashed command paths
+# Set the default file permissions to user and group read/write/execute, etc.
+umask 002
 
 # Update and upgrade system packages (using apt)
 echo "Updating and upgrading system..."
@@ -14,7 +28,7 @@ echo "Installing nala..."
 sudo apt install nala -y
 
 # Use nala to update and upgrade
-sudo nala update && sudo nala upgrade -y
+sudo nala upgrade --update -y
 
 # Remove w3m
 echo "Removing w3m..."
@@ -32,6 +46,7 @@ git clone --depth=1 https://github.com/junegunn/fzf.git ~/.fzf
 # Install NeoVim from source
 echo "Installing NeoVim dependencies..."
 sudo nala install ninja-build gettext cmake unzip curl build-essential -y
+sudo nala upgrade --update -y
 echo "Installing NeoVim..."
 git clone https://github.com/neovim/neovim
 cd neovim
@@ -43,21 +58,24 @@ cd ..
 echo "Installing Bun and Java..."
 curl https://setup.bun.sh | bash
 sudo nala install default-jdk -y
+sudo nala upgrade --update -y
 
 # Install Rust via rustup
 echo "Installing Rust..."
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+source ~/.cargo/env # Setup the Rust and Cargo environment
 
 # Install nushell, zoxide, and oh-my-posh
-source ~/.bashrc
-source ~/.cargo/env
+# source ~/.bashrc
 echo "Installing nushell dependencies..."
 sudo nala install pkg-config libssl-dev build-essential -y
+sudo nala upgrade --update -y
 echo "Installing nushell..."
 cargo install nu
 echo "Initializing nushell..."
-nu
 exit
+nu
+
 echo "Installing zoxide and oh-my-posh..."
 cargo install zoxide
 curl -s https://ohmyposh.dev/install.sh | bash -s
